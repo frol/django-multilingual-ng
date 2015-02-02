6290cedcae4214f37194f76e9bdb21fbf5b2a101
frol <frolvlad@gmail.com>
2015-02-02 12:42:44 +0200

    Added optional Jinja2 support

--- multilingual/mlflatpages/views.py
+++ multilingual/mlflatpages/views.py
@@ -1,8 +1,15 @@
 from django.conf import settings
-from django.contrib.flatpages.views import render_flatpage
 from django.contrib.sites.models import get_current_site
-from django.http import Http404, HttpResponsePermanentRedirect
+from django.http import Http404, HttpResponse, HttpResponsePermanentRedirect
 from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_protect
+
+if 'coffin' in settings.INSTALLED_APPS:
+    from coffin.template import loader, RequestContext
+    from jinja2 import Markup as mark_safe
+else:
+    from django.template import loader, RequestContext
+    from django.utils.safestring import mark_safe
 
 from .models import FlatPage
 
@@ -42,3 +49,30 @@ def flatpage(request, url):
 
     # Use django flatpage render
     return render_flatpage(request, f)
+
+@csrf_protect
+def render_flatpage(request, f):
+    """
+    Internal interface to the flat page view.
+    """
+    # If registration is required for accessing this page, and the user isn't
+    # logged in, redirect to the login page.
+    if f.registration_required and not request.user.is_authenticated():
+        from django.contrib.auth.views import redirect_to_login
+        return redirect_to_login(request.path)
+    if f.template_name:
+        t = loader.select_template((f.template_name, DEFAULT_TEMPLATE))
+    else:
+        t = loader.get_template(DEFAULT_TEMPLATE)
+
+    # To avoid having to always use the "|safe" filter in flatpage templates,
+    # mark the title and content as already safe (since they are raw HTML
+    # content in the first place).
+    f.title = mark_safe(f.title)
+    f.content = mark_safe(f.content)
+
+    c = RequestContext(request, {
+        'flatpage': f,
+    })
+    response = HttpResponse(t.render(c))
+    return response
